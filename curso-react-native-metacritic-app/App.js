d61916c924472ce86de1ebdfb264ea587e763f7d
Camilo Bolaños <camiloa.andresc@hotmail.com>
2025-04-25 22:44:33 -0500

    feat: implement game fetching functionality and update App component

--- curso-react-native-metacritic-app/App.js
+++ curso-react-native-metacritic-app/App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { getLatestGames } from "./lib/metacritic";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [games, setGames] = useState([]);
+
+	useEffect(() => {
+		getLatestGames().then((games) => {
+			setGames(games);
+		});
+	}, []);
+	return (
+		<View style={styles.container}>
+			<StatusBar style="light" />
+			<Text>Hola asdadas adsad</Text>
+			{games.map((game) => (
+				<View key={game.slug} style={styles.card}>
+					<Image
+						source={{ uri: game.image }}
+						style={{ with: 107, height: 147, borderRadius: 10 }}
+					/>
+				</View>
+			))}
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#000",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
